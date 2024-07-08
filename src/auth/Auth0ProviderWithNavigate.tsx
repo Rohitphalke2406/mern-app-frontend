2c267d611bfee1f6c0eda3c7e71f2c95a88caafd
Rohit Phalke <rohitphalke165@gmail.com>
2024-07-08 19:58:29 +0530

    Order page added

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import {  Auth0Provider } from "@auth0/auth0-react";
+import {  AppState, Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 // import { useCreateMyUser } from "../api/MyUserApi";
 
@@ -18,9 +18,9 @@ const Auth0ProviderWithNavigate = ({children}: Props) => {
         throw new Error("Unable to initialize auth");
     }
 
-    const onRedirectCallback = () => {
+    const onRedirectCallback = (appState?: AppState) => {
         // console.log("User" , user);
-        navigate("/auth-callback");
+        navigate( appState?.returnTo || "/auth-callback");
       
     };
 
