eeaadeb0ea086dc3d83ffb2b07e474d7154b05a5
Rohit Phalke <rohitphalke165@gmail.com>
2024-07-18 17:35:07 +0530

    changes in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="bg-green-500 py-5 ">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center ">
             <span className="text-3xl text-white tracking-tight">
-                MernEats.com
+                FoodieExpress
             </span>
             <span className="text-white font-bold tracking-tight flex gap-4">
                 <span>Privacy Policy</span>
